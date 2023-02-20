4dd79da1fbdcbf35d0c02c616397c0206a173943
Alex Hetherington <alexjhetherington@gmail.com>
2023-02-20 20:42:14 +0000

    Git ignore and tags

---
+++ Assets/Plugins/FootnoteUtilities/FootnoteUtilities/Tags/TagExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class TagExtensions
+{
+    public static bool HasTag(this Component component, Tag tag)
+    {
+        var tags = component.GetComponent<Tags>();
+        if (tags != null)
+            return tags.tags.Contains(tag);
+
+        return false;
+    }
+}
