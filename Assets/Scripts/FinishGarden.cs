3f490da3b85aa4947ce8bbad084e37ae0424f6fe
Thomas Watkins <tawatkins92@gmail.com>
2023-02-21 23:14:02 +0000

    Basic finish ui

--- Assets/Scripts/FinishGarden.cs
+++ Assets/Scripts/FinishGarden.cs
@@ -6,6 +6,12 @@ public class FinishGarden : MonoBehaviour
 {
     public IntVariable poopsCleared_so;
 
+    public float uiYPos = 50f;
+    public UISettings uiSettings;
+
+    private RectTransform currentUI;
+    private string CootsMood = "Happy";
+
     // Update is called once per frame
     void Update()
     {
@@ -15,8 +21,30 @@ public class FinishGarden : MonoBehaviour
             //trigger coots animator
             //bring up ui for end/regen
             Debug.Log("score is : " + poopsCleared_so.Value);
+            ShowFinishUI();
             //resetting here but need to put this in the start/restart logic
             poopsCleared_so.Value = 0;
         }
+    }
+
+    public void ShowFinishUI()
+    {
+        var ui = uiSettings
+            .MakeUi(AnchorUtil.BottomCentre(uiYPos))
+            .AddChildren(
+                uiSettings.Text("You've completed your garden and now Coots can poop freely"),
+                uiSettings.Text("Poops seems to be " + CootsMood), 
+                //reload scene
+                uiSettings.Button("Play Again", () => Transitions.Start("SimpleFade", "Game")),
+                uiSettings.Button("Quit", () => Application.Quit())
+            );
+
+        currentUI = ui;
+    }
+
+    public void ClearFinishUI()
+    {
+        UISettings.DestroyUi(currentUI);
+        currentUI = null;
     }
 }
