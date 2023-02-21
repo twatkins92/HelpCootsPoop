dc40c42eb2143d8790b5634fa9aeda43b28aad58
Alex Hetherington <alexjhetherington@gmail.com>
2023-02-21 23:30:52 +0000

    I'm not a digger mate.

--- Assets/Scripts/DryGarden/MeshClicker.cs
+++ Assets/Scripts/DryGarden/MeshClicker.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using DataStructures.ViliWonka.KDTree;
 using UnityEngine;
 
-public class MeshClicker : MonoBehaviour
+public class MeshClicker : Diggable
 {
     public struct Brush
     {
@@ -68,61 +68,52 @@ public class MeshClicker : MonoBehaviour
         kdTree = GetComponent<KDT>();
     }
 
-    void OnMouseOver()
+    public override void TryDig(Vector3 vector)
     {
-        if (Input.GetMouseButton(0))
+        Vector3 zeroedIntersectPoint = vector.Horizontal();
+        if (!mesh.bounds.Contains(zeroedIntersectPoint))
+            return;
+
+        if (drawnPoints.Count > 0 && Vector3.Distance(zeroedIntersectPoint, drawnPoints[^1]) < 0.1f)
+            return;
+
+        if (drawnPoints.Count == 0)
         {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-
-            float intersectDistance = default;
-            new Plane(Vector3.up, Vector3.zero).Raycast(ray, out intersectDistance);
-            Vector3 zeroedIntersectPoint = ray.origin + ray.direction * intersectDistance;
-
-            if (
-                drawnPoints.Count > 0
-                && Vector3.Distance(zeroedIntersectPoint, drawnPoints[^1]) < 0.1f
-            )
-                return;
-
-            if (drawnPoints.Count == 0)
+            drawnPoints.Add(zeroedIntersectPoint);
+        }
+        else
+        {
+            int startIndex = drawnPoints.Count - 1;
+            Vector3 diff = (zeroedIntersectPoint - drawnPoints[^1]);
+            float distance = diff.magnitude;
+            int numMidPoints = Mathf.FloorToInt(distance / (0.4f * brushLength));
+            Vector3 nextPoint = drawnPoints[startIndex];
+            for (int i = 0; i < numMidPoints; i++)
             {
-                drawnPoints.Add(zeroedIntersectPoint);
+                nextPoint += 0.4f * brushLength * diff.normalized;
+                drawnPoints.Add(nextPoint);
             }
-            else
+            drawnPoints.Add(zeroedIntersectPoint);
+
+            for (int i = startIndex + 1; i < drawnPoints.Count; i++)
             {
-                int startIndex = drawnPoints.Count - 1;
-                Vector3 diff = (zeroedIntersectPoint - drawnPoints[^1]);
-                float distance = diff.magnitude;
-                int numMidPoints = Mathf.FloorToInt(distance / (0.4f * brushLength));
-                Vector3 nextPoint = drawnPoints[startIndex];
-                for (int i = 0; i < numMidPoints; i++)
+                //Vector3 centre = (drawnPoints[^1] + drawnPoints[^2]) / 2;
+                Vector3 centre = drawnPoints[i];
+
+                int nearestVertexIndex = FindNearestVertex(centre);
+
+                // TODO Rather than just face forward, face the direction the mouse is being dragged
+                // Make sure it is normalised
+                Vector3 forwardDirection = (drawnPoints[i] - drawnPoints[i - 1]).normalized;
+                Vector3 rightDirection = Quaternion.Euler(0, 90, 0) * forwardDirection.normalized;
+
+                Brush brush = new Brush
                 {
-                    nextPoint += 0.4f * brushLength * diff.normalized;
-                    drawnPoints.Add(nextPoint);
-                }
-                drawnPoints.Add(zeroedIntersectPoint);
-
-                for (int i = startIndex + 1; i < drawnPoints.Count; i++)
-                {
-                    //Vector3 centre = (drawnPoints[^1] + drawnPoints[^2]) / 2;
-                    Vector3 centre = drawnPoints[i];
-
-                    int nearestVertexIndex = FindNearestVertex(centre);
-
-                    // TODO Rather than just face forward, face the direction the mouse is being dragged
-                    // Make sure it is normalised
-                    Vector3 forwardDirection = (drawnPoints[i] - drawnPoints[i - 1]).normalized;
-                    Vector3 rightDirection =
-                        Quaternion.Euler(0, 90, 0) * forwardDirection.normalized;
-
-                    Brush brush = new Brush
-                    {
-                        centre = mesh.vertices[nearestVertexIndex],
-                        direction = forwardDirection * (brushLength / 2),
-                        right = rightDirection * (brushWidth / 2)
-                    };
-                    RaiseVertices(brush);
-                }
+                    centre = mesh.vertices[nearestVertexIndex],
+                    direction = forwardDirection * (brushLength / 2),
+                    right = rightDirection * (brushWidth / 2)
+                };
+                RaiseVertices(brush);
             }
         }
     }
@@ -199,24 +190,24 @@ public class MeshClicker : MonoBehaviour
             return Mathf.Max(prevHeight, heightNoBlend);
     }
     /*void SmoothVertex(int index)
-    {
-        // Get the neighboring vertices of the vertex to be moved
-        int[] neighborIndices = mesh.GetTriangles(index);
-        Vector3[] neighbors = ;
-        for (int i = 0; i < neighborIndices.Length; i++)
-        {
-            neighbors[i] = vertices[neighborIndices[i]];
-        }
+{
+   // Get the neighboring vertices of the vertex to be moved
+   int[] neighborIndices = mesh.GetTriangles(index);
+   Vector3[] neighbors = ;
+   for (int i = 0; i < neighborIndices.Length; i++)
+   {
+       neighbors[i] = vertices[neighborIndices[i]];
+   }
 
-        // Calculate the average position of the neighboring vertices
-        Vector3 averagePosition = Vector3.zero;
-        for (int i = 0; i < neighbors.Length; i++)
-        {
-            averagePosition += neighbors[i];
-        }
-        averagePosition /= neighbors.Length;
+   // Calculate the average position of the neighboring vertices
+   Vector3 averagePosition = Vector3.zero;
+   for (int i = 0; i < neighbors.Length; i++)
+   {
+       averagePosition += neighbors[i];
+   }
+   averagePosition /= neighbors.Length;
 
-        // Move the vertex towards the average position
-        vertices[index] = Vector3.Lerp(vertices[index], averagePosition, 0.5f);
-    }*/
+   // Move the vertex towards the average position
+   vertices[index] = Vector3.Lerp(vertices[index], averagePosition, 0.5f);
+}*/
 }
