ddb85d3550916b57c0c5dc824127c6d0efd7769f
Thomas Watkins <tawatkins92@gmail.com>
2023-02-21 21:28:22 +0000

    Add initial script for player finishing garden. Need to link to some UI elements and coots animations

---
+++ Assets/Scripts/FinishGarden.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinishGarden : MonoBehaviour
+{
+    public IntVariable poopsCleared_so;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            //calculate score
+            //trigger coots animator
+            //bring up ui for end/regen
+            Debug.Log("score is : " + poopsCleared_so.Value);
+        }
+    }
+}
